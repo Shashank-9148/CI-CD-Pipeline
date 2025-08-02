5ce39bc72d16fcf03ff9aac0f5b365961b3075c4
Shashank-9148 <shashanklokesh2001@gmail.com>
2025-08-02 08:53:24 +0530

    Changed return statement

--- pipeline-practice/main.py
+++ pipeline-practice/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from DevOps Engineering Team!"
+    return "Hello from DevOps  RIS Engineering Team!"
 
 
 if __name__ == '__main__':
